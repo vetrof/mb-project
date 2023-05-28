3c964ff1345bd4cdb03f38c8132c2938f12cdac5
Vitaly Vetrof <vetrof@gmail.com>
2023-05-28 16:06:41 +0600

    update

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from posts.models import Post
+
+
+class PostModeTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='just a test')
+
+    def test_text_content(self):
+        post=Post.objects.get(id=1)
+        expected_object_name = f'{post.text}'
+        self.assertEquals(expected_object_name, 'just a test')
+
+
+class HomePageViewTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='just a test')
+
+    def test_view_url_exists(self):
+        resp = self.client.get('/')
+        self.assertEquals(resp.status_code, 200)
+
+    def test_view_url_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEquals(resp.status_code, 200)
